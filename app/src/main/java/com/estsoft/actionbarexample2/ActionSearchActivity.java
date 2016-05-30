554263c9fe461dc54dc87e6a21e0569798a1e2e6
kickscar <kickscar@gmail.com>
2016-05-30 10:51:05 +0900

    2nd

--- app/src/main/java/com/estsoft/actionbarexample2/ActionSearchActivity.java
+++ app/src/main/java/com/estsoft/actionbarexample2/ActionSearchActivity.java
@@ -3,6 +3,7 @@ package com.estsoft.actionbarexample2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ActionMode;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -20,13 +21,28 @@ public class ActionSearchActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
         menu.
-                add("Search").
+                add( Menu.NONE, 0, 0, "Search").
                 setIcon( android.R.drawable.ic_menu_search ).
                 setActionView( R.layout.collapsible_edittext ).
                 setShowAsAction( MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW );
 
+        MenuItem menuItem = menu.findItem(0);
+        final EditText searchText = (EditText) menuItem.getActionView();
+
+        searchText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if( actionId == EditorInfo.IME_ACTION_SEARCH ) {
+                   Log.d( "------->", v.getText().toString() );
+                   v.setText( "" );
+                }
+                return false;
+            }
+        });
+
         return true;
     }
+
+
 }
